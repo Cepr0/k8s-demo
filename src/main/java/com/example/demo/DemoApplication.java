2df8e3c7fd09b57612783c352d8f4334a1b698a7
Sergei Poznanski <cepr0@ukr.net>
2020-08-02 21:44:56 +0300

    Add 'hello' endpoint

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +15,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping("/hello")
+	public Map<String, String> hello() {
+		return Map.of("message", "Hello World!");
+	}
 }
